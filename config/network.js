e478f7e2319fa2ebf533b552faf2e3c015bb1b9d
jash413 <jashmistry4444@gmail.com>
2024-01-23 13:33:22 +0530

    CHANGES

--- config/network.js
+++
@@ -1,5 +0,0 @@
-const network = {
-    baseURL: 'https://trakky.in:8000',
-}
-
-export default network;
--- image-loader.js
+++
@@ -1,3 +0,0 @@
-export default function cloudinaryLoader({ src }) {
-  return `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${src}`;
-}
