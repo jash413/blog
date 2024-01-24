2e7d08cc04f7514f0f9bb19210b8ab65b70eab4e
jash413 <jashmistry4444@gmail.com>
2024-01-24 12:11:39 +0530

    changes

--- image-loader.js
+++ image-loader.js
@@ -1,3 +1,3 @@
 export default function cloudinaryLoader({ src }) {
-  return `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${src}`;
+  return `https://res.cloudinary.com/dvjozqf7u/image/upload/${src}`;
 }
